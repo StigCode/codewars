aa7b26b831984186e631b67c9e5429566ffd1f9f
StigCode <jstig4@gmail.com>
2023-02-15 14:44:27 -0800

    Add findShort.js solution

---
+++ 7kyu/findShort.js
@@ -0,0 +1,12 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// MY CODE
+
+function findShort(s) {
+  return s.split(" ").reduce((prev, curr) => {
+    if (curr.length < prev) return curr.length;
+    return prev;
+  }, Infinity);
+}
