734d899cf39c26c2098ca254df75da9eef0fe455
StigCode <jstig4@gmail.com>
2023-02-18 21:02:54 -0800

    Add findOdd.js solution

---
+++ 6yu/findOdd.js
@@ -0,0 +1,28 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+// Examples
+
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+// MY CODE
+
+function findOdd(arr) {
+  let count = {};
+
+  arr.forEach((el) => {
+    if (count[el]) {
+      count[el] += 1;
+    } else {
+      count[el] = 1;
+    }
+  });
+
+  for (const [key, value] of Object.entries(count)) {
+    if (value % 2 !== 0) return Number(key);
+  }
+}
